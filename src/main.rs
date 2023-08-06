337173146f40eb82847c7443bacc2cc040615a15
lukasvrenner <lukasvrenner@icloud.com>
2023-08-06 11:04:20 -0800

    largely improved design to allow for more options, and fixes issue in -n option

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,22 @@ struct Args {
     #[arg(short, long, default_value_t = false)]
     number: bool,
 
+    /// Supress repeated empty output lines
+    #[arg(short, long, default_value_t = false)]
+    squeeze_blank: bool,
+
 
 }
 
 fn main() {
     let args = Args::parse();
-    let contents = read_file(&args.file).unwrap_or_else(|err| {
+    let mut contents = read_file(&args.file).unwrap_or_else(|err| {
         eprintln!("error: could not read file: {err}");
         process::exit(1);
 
     });
-    let output = apply_options(&contents, &args);
-    println!("{output}");
+    apply_options(&mut contents, &args);
+    print!("{contents}");
 }
 
 // reads text file
@@ -34,17 +38,46 @@ fn read_file(file: &String) -> Result<String, Box<dyn Error>> {
     Ok(contents)
 }
 
-fn apply_options(file_contents: &str, args: &Args) -> String {
+fn apply_options(file_contents: &mut String, args: &Args) -> String {
     if args.number {
-        file_contents
-            .lines()
-            .enumerate()
-            .map(|(i, line)| format!("{i}  {line}\n"))
-            .collect()
+        *file_contents = number(file_contents);
            
-    } else {
-        file_contents.to_string()
+    } 
+    if args.squeeze_blank {
+        *file_contents = squeeze_blank(file_contents);
     }
+        file_contents.to_string()
+    
 }
 
+fn number(text: &str) -> String {
+    text
+        .lines()
+        .enumerate()
+        .map(|(i, line)| format!("{}  {line}\n", i+1))
+        .collect()
+}
 
+fn squeeze_blank(text: &str) -> String {
+    todo!();
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     let file_contents: String = String::from("
+// This is a string
+//
+// It has multiple lines
+//
+//
+// It even has larger gaps
+//                                                  ");
+//     assert_eq!(number(file_contents), String::from("
+// 1  This is a string
+// 2  
+// 3  It has multiple lines
+// 4
+// 5  It even has larger gaps
+//                                                    "));
+// }
